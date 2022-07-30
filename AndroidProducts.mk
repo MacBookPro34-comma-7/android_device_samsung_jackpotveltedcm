b461f1f4e832b06bd17d316b4a108d1b4404be9a
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-07-30 19:20:39 +0900

    jackpotveltedcm: import initial jackpotveltedcm device tree.

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_jackpotveltedcm.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_jackpotveltedcm-user \
+    omni_jackpotveltedcm-userdebug \
+    omni_jackpotveltedcm-eng
---
+++ recovery/root/sbin/permissive.sh
@@ -0,0 +1,3 @@
+#!/sbin/sh
+
+setenforce 0
