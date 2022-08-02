63f957dc3a39879d37a8350c455b2fd5bb06afc3
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-08-02 13:08:27 +0000

    jackpotveltedcm: the initial works of the building recovery.

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+add_lunch_combo omni_jackpotveltedcm-user
+add_lunch_combo omni_jackpotveltedcm-userdebug
+add_lunch_combo omni_jackpotveltedcm-eng
